64504efd1693a74c3603217b6a56fbdc307f3e58
Riyadulislamratul <riadul1810833319@gmail.com>
2026-03-27 23:51:45 +0600

    Tailwind router dom added

---
+++ src/components/Root.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import { Outlet } from 'react-router'
+import Footer from './Footer'
+
+const Root = () => {
+  return (
+    <main>
+        <Header />
+        <Outlet />
+        <Footer />
+    </main>
+  )
+}
+
+export default Root
